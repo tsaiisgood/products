aa4c20de8e890cf564caf573060b1ae84a529c1a
Amy <tingrou880603@gmail.com>
2023-08-16 13:47:00 +0800

    add read file

--- prouducts.py
+++ prouducts.py
@@ -1,24 +1,32 @@
-proudcts=[]
+products = []
 
+# 從 CSV 檔案讀取商品
+with open('products.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue  # 跳過標題行
+        name, price = line.strip().split(',')
+        products.append([name, price])
+
+print(products)
+
+# 輸入新的商品
 while True:
-	name = input('請輸入商品名')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格')
+    name = input('請輸入商品名稱: ')
+    if name == 'q':
+        break
+    price = input('請輸入商品價格: ')
+    price = int(price)
+    products.append([name, price])
 
-	price= int(price)
-	
-	#p=[name,price]
-	#p.append(name)
-	#p.append(price)
-	proudcts.append([name,price])
+print(products)
 
-print(proudcts)
+# 印出所有商品及其價格
+for p in products:
+    print(p[0], '的價格是', p[1])
 
-for p in proudcts:
-	print(p[0],'的價格是',p[1])
-
-with open('products.csv','w',encoding='utf-8') as f:
-	f.write('商品,價格\n')
-	for p in proudcts:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+# 將更新的商品列表寫回 CSV 檔案
+with open('products.csv', 'w', encoding='utf-8') as f:
+    f.write('商品,價格\n')
+    for p in products:
+        f.write(p[0] + ',' + str(p[1]) + '\n')
